caa849cddb3f8ac261c33248f085389b4495514a
inboundbark <inboundbark49@gmail.com>
2026-03-01 16:08:19 +0100

    Add old mineshaft frequency mixin

---
+++ src/main/java/net/inboundbark/classiccaves/mixins/MineshaftStructureMixin.java
@@ -0,0 +1,30 @@
+package net.inboundbark.classiccaves.mixins;
+
+import net.minecraft.structure.MineshaftStructure;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MineshaftStructure.class)
+public class MineshaftStructureMixin {
+    @Shadow
+    private double chance;
+
+    @Inject(
+            method = "<init>*",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/structure/MineshaftStructure;chance:D",
+                    opcode = Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER,
+                    ordinal = 0 // Only the first assign
+            )
+    )
+    private void revertChance(CallbackInfo ci) {
+        this.chance = 0.01D;
+    }
+
+}
