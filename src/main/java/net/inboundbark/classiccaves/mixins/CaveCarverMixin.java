2335651bf987cbc401fd2fb784a177a29c17eb5e
inboundbark <inboundbark49@gmail.com>
2026-03-01 16:08:19 +0100

    Replace ModifyConstant with ModifyExpressionValue

--- src/main/java/net/inboundbark/classiccaves/mixins/CaveCarverMixin.java
+++ src/main/java/net/inboundbark/classiccaves/mixins/CaveCarverMixin.java
@@ -1,24 +1,24 @@
 package net.inboundbark.classiccaves.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.gen.carver.CaveCarver;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 
 @Mixin(CaveCarver.class)
 public class CaveCarverMixin {
-    @ModifyConstant(
+    @ModifyExpressionValue(
             method = "carve",
-            constant = @Constant(intValue = 15, ordinal = 0))
+            at = @At(value = "CONSTANT", args = "intValue=15", ordinal = 0))
     private int return40(int integer) {
         return 40;
     }
 
-    @ModifyConstant(
+    @ModifyExpressionValue(
             method = "carve",
-            constant = @Constant(intValue = 7, ordinal = 0))
+            at = @At(value = "CONSTANT", args = "intValue=7", ordinal = 0))
     private int return15(int integer) {
         return 15;
     }
