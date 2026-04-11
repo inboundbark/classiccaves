bc62f518875fdba7a1fb420ebc41e82d81e6c87b
inboundbark <inboundbark49@gmail.com>
2026-04-11 19:17:17 +0200

    remove code duplication in mineshaft code
    the kind people of gtnh discord showed me that method can take an array.
    Cool!

--- src/main/java/com/inboundbark/classiccaves/mixins/MapGenMineshaftMixin.java
+++ src/main/java/com/inboundbark/classiccaves/mixins/MapGenMineshaftMixin.java
@@ -1,6 +1,5 @@
 package com.inboundbark.classiccaves.mixins;
 
-import io.github.tox1cozz.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.gen.structure.MapGenMineshaft;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -8,23 +7,16 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import com.inboundbark.classiccaves.Config;
 
+import io.github.tox1cozz.mixinextras.injector.ModifyExpressionValue;
+
 @Mixin(MapGenMineshaft.class)
 public class MapGenMineshaftMixin {
 
     @ModifyExpressionValue(
-        method = "<init>()V",
+        method = { "<init>()V", "<init>(Ljava/util/Map;)V" },
         at = @At(value = "CONSTANT", args = "doubleValue=0.004", ordinal = 0 // Only the first assign
         ))
-    private double revertChance1(double chance) {
+    private double revertChance(double chance) {
         return Config.higherMineshaftChance ? 0.01D : chance;
     }
-
-    @ModifyExpressionValue(
-        method = "<init>(Ljava/util/Map;)V",
-        at = @At(value = "CONSTANT", args = "doubleValue=0.004", ordinal = 0 // Only the first assign
-        ))
-    private double revertChance2(double chance) {
-        return Config.higherMineshaftChance ? 0.01D : chance;
-    }
-    // Annoying code duplication because it didn't understand the wildcard "<init>*"
 }
