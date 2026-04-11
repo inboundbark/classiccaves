528553cf3647743d13b894aa0545d0a5123125df
inboundbark <inboundbark49@gmail.com>
2026-04-11 15:54:59 +0200

    add old mineshaft spawning

--- src/main/java/com/inboundbark/classiccaves/Config.java
+++ src/main/java/com/inboundbark/classiccaves/Config.java
@@ -7,12 +7,16 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
 
     public static boolean higherCaveFrequency = true;
+    public static boolean higherMineshaftChance = true;
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
 
         higherCaveFrequency = configuration
             .getBoolean("higherCaveFrequency", Configuration.CATEGORY_GENERAL, higherCaveFrequency, "enable pre r1.7 cave frequency?");
+
+        higherMineshaftChance = configuration
+            .getBoolean("higherMineshaftChance", Configuration.CATEGORY_GENERAL, higherMineshaftChance, "enable pre r1.7 mineshaft frequency?");
 
         if (configuration.hasChanged()) {
             configuration.save();
---
+++ src/main/java/com/inboundbark/classiccaves/mixins/MapGenMineshaftMixin.java
@@ -0,0 +1,32 @@
+package com.inboundbark.classiccaves.mixins;
+
+import com.inboundbark.classiccaves.Config;
+import net.minecraft.world.gen.structure.MapGenMineshaft;
+
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MapGenMineshaft.class)
+public class MapGenMineshaftMixin {
+
+    @Shadow
+    private double field_82673_e;
+
+    @Inject(
+        method = "<init>*",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/world/gen/structure/MapGenMineshaft;field_82673_e:D",
+            opcode = Opcodes.PUTFIELD,
+            shift = At.Shift.AFTER,
+            ordinal = 0 // Only the first assign
+        ))
+    private void revertChance(CallbackInfo ci) {
+        this.field_82673_e = Config.higherMineshaftChance ? 0.01D : this.field_82673_e;
+    }
+
+}
